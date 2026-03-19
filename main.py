3a1e19928c51ca9e7fb72571a12cab4695dae30e
Vineet Gaira <vineetgaira1729@gmail.com>
2026-03-19 19:27:21 +0530

    Added a winnings feature

--- main.py
+++ main.py
@@ -13,6 +13,31 @@ symbols_count={
     "C":6,
     "D":8
 }
+symbols_value={
+    "A":5,
+    "B":4,
+    "C":3,
+    "D":2
+}
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+
+    return winnings,winning_lines
+
+
+    
+
 
 def get_slot_machine_spin(rows, cols, symbol):
     all_symbols = []
@@ -25,7 +50,7 @@ def get_slot_machine_spin(rows, cols, symbol):
         column = []
         current_symbols = all_symbols[:]
         for _ in range(rows):
-            value = random.choice(all_symbols)
+            value = random.choice(current_symbols)
             current_symbols.remove(value)
             column.append(value)
 
@@ -85,10 +110,8 @@ def get_bet():
             print("Please enter a valid amount.")
     return bet_amount
 
-
-def main():
-    balance = deposit()
-    lines=get_num_lines()
+def spin():
+    lines = get_num_lines()
     while True:
         bet=get_bet()
         total_bet=lines*bet
@@ -101,5 +124,25 @@ def main():
 
     slots = get_slot_machine_spin(ROWS, COLS, symbols_count)
     print_slot_machine(slots)
-    
+
+    winnings, winning_lines = check_winnings(slots, lines, lines, symbols_value)
+    print(f"You won ₹{winnings}.")
+    print(f"You won on lines : ", *winning_lines)
+    return winnings - total_bet 
+
+
+def main():
+    balance=deposit()
+    while True:
+        print(f"Current balance is ₹{balance}.")
+        user_answer = input("Press enter to play ('q' to quit).")
+        if user_answer == "q":
+            break
+        balance += spin(balance=deposit())
+    print(f"You left with ₹{balance}.")
+
 main()
+
+
+
+    
