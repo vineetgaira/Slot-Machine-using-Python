236ba35a8723f21d039a02f4305349000fc10771
Vineet Gaira <vineetgaira1729@gmail.com>
2026-03-19 17:00:33 +0530

    Added a new feature to spin and print the slot

--- main.py
+++ main.py
@@ -1,7 +1,49 @@
+import random
 
 MAX_LINES=3
 MAX_BET=1000
 MIN_BET=10
+
+ROWS=3
+COLS=3
+
+symbols_count={
+    "A":2,
+    "B":4,
+    "C":6,
+    "D":8
+}
+
+def get_slot_machine_spin(rows, cols, symbol):
+    all_symbols = []
+    for symbol,symbol_count in symbol.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+    
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(all_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) -1 :
+                print(column[row], end=" | ")
+            else:
+                print(column[row],end="")
+            
+        print()
+
+
 
 def deposit():
     while True:
@@ -56,4 +98,8 @@ def main():
         else:
             break
     print(f"Balance : ₹{balance}\tLines : {lines} lines.\nTotal bet is : ₹{total_bet}." )
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbols_count)
+    print_slot_machine(slots)
+    
 main()
